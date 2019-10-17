c88e00a85954e2791ab2d298c9c1f849bdd0b48b
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-17 18:30:00 +0100

    completed Kata4

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,12 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    let arr = [];
+    const catYears = (24) + (4*(number-2));
+    const dogYears = (24) + (5*(number-2));
+arr = [number, catYears, dogYears];
+return arr;
+};
 
 module.exports = humanCatDogYears;
+
+//cat years = number + 15, number + 9, number + (4*(number-2))
+//dog years = number + 15, number +9, number + (5*(number-2))
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,15 @@
 const { humanCatDogYears } = require("../src");
 
+describe("humanCatDogYears", () => {
+    it("returns an array",  () => {
+        const total = humanCatDogYears()
+        expect(Array.isArray(total)).toBe(true);
+    })
+    it("returns human, cat and dog years", () => {
+        expect(humanCatDogYears(10)).toStrictEqual([10, 56, 64]);
+    })
+})
+
 // Look Ma, no handlebars!!!
+//returns an array with three elements
+//returns the correct numbers - example tests
